839aee396eecaa6861bfa52a4d2e82f1768624a9
Hermann Krumrey <hermann@krumreyh.com>
2017-01-10 22:25:46 +0100

    Identify Pins

--- dice.py
+++ dice.py
@@ -4,24 +4,33 @@ import time
 from RPi.GPIO import BCM, OUT, IN, HIGH, LOW
 from RPi.GPIO import setup, setmode, setwarnings, output
 
-LEFT_TOP = 17
-LEFT_MID = 0
-LEFT_BOT = 0
-RIGHT_TOP = 0
-RIGHT_MID = 0
-RIGHT_BOT = 0
+LEFT_TOP = 22
+LEFT_MID = 27
+LEFT_BOT = 17
+RIGHT_TOP = 2
+RIGHT_MID = 3
+RIGHT_BOT = 4
+BUTTON = 14
 
 def setup_gpio():
 	setmode(BCM)
 	setwarnings(False)
 
 	setup(LEFT_TOP, OUT)
+	setup(LEFT_MID, OUT)
+	setup(LEFT_BOT, OUT)
+	setup(RIGHT_TOP, OUT)
+	setup(RIGHT_MID, OUT)
+	setup(RIGHT_BOT, OUT)
+	setup(BUTTON, IN)
 
 if __name__ == "__main__":
 	setup_gpio()
 
-	output(LEFT_TOP, HIGH)
+	for i in [22,27,17,2,3,4]:
 
-	time.sleep(1)
+		output(i, HIGH)
 
-	output(LEFT_TOP, LOW)
+		time.sleep(1)
+
+		output(i, LOW)
