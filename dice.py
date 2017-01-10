a82a4e710180e8cb19aff36057d06424647117f4
Hermann Krumrey <hermann@krumreyh.com>
2017-01-10 22:41:29 +0100

    Add Dice Roll functionality

--- dice.py
+++ dice.py
@@ -1,40 +1,45 @@
 #!/usr/bin/env python
 
 import time
+import random
 from RPi.GPIO import BCM, OUT, IN, HIGH, LOW, PUD_UP
 from RPi.GPIO import setup, setmode, setwarnings, output
 from RPi.GPIO import input as _input
 
-LEFT_TOP = 22
-LEFT_MID = 27
-LEFT_BOT = 17
-RIGHT_TOP = 2
-RIGHT_MID = 3
-RIGHT_BOT = 4
+LEDS = [22, 27, 17, 2, 3, 4]
 BUTTON = 14
 
 def setup_gpio():
     setmode(BCM)
     setwarnings(False)
 
-    setup(LEFT_TOP, OUT)
-    setup(LEFT_MID, OUT)
-    setup(LEFT_BOT, OUT)
-    setup(RIGHT_TOP, OUT)
-    setup(RIGHT_MID, OUT)
-    setup(RIGHT_BOT, OUT)
+    for led in LEDS:
+        setup(led, OUT)
+
     setup(BUTTON, IN, pull_up_down=PUD_UP)
 
+def wait_for_button_press():
+    while _input(BUTTON):
+        pass
+
+def randomize(led):
+
+    if random.randint(0, 1) == 1:
+        output(led, HIGH)
+    else:
+        output(led, LOW)
+
 if __name__ == "__main__":
+    
     setup_gpio()
 
-    for i in [22,27,17,2,3,4]:
-
-        output(i, HIGH)
-        time.sleep(1)
-        output(i, LOW)
-
     while True:
-        if _input(BUTTON) == False:
-            print('Button Pressed')
-            time.sleep(0.2)
+
+        wait_for_button_press()
+
+        end_time = time.time() + 10
+
+        while time.time() < end_time:
+            for led in LEDS:
+                randomize(led)
+
