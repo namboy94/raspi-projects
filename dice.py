bd9f0d2abba3099307eee4ebb65a140f1fc8d371
Hermann Krumrey <hermann@krumreyh.com>
2017-01-10 22:34:18 +0100

    Unify Indentation

--- dice.py
+++ dice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import time
-from RPi.GPIO import BCM, OUT, IN, HIGH, LOW
+from RPi.GPIO import BCM, OUT, IN, HIGH, LOW, PUD_UP
 from RPi.GPIO import setup, setmode, setwarnings, output
 from RPi.GPIO import input as _input
 
@@ -14,27 +14,27 @@ RIGHT_BOT = 4
 BUTTON = 14
 
 def setup_gpio():
-	setmode(BCM)
-	setwarnings(False)
+    setmode(BCM)
+    setwarnings(False)
 
-	setup(LEFT_TOP, OUT)
-	setup(LEFT_MID, OUT)
-	setup(LEFT_BOT, OUT)
-	setup(RIGHT_TOP, OUT)
-	setup(RIGHT_MID, OUT)
-	setup(RIGHT_BOT, OUT)
-	setup(BUTTON, IN)
+    setup(LEFT_TOP, OUT)
+    setup(LEFT_MID, OUT)
+    setup(LEFT_BOT, OUT)
+    setup(RIGHT_TOP, OUT)
+    setup(RIGHT_MID, OUT)
+    setup(RIGHT_BOT, OUT)
+    setup(BUTTON, IN, pull_up_down=PUD_UP)
 
 if __name__ == "__main__":
-	setup_gpio()
+    setup_gpio()
 
-	for i in [22,27,17,2,3,4]:
+    for i in [22,27,17,2,3,4]:
 
-		output(i, HIGH)
-		time.sleep(1)
-		output(i, LOW)
+        output(i, HIGH)
+        time.sleep(1)
+        output(i, LOW)
 
-	while True:
-    	if _input(BUTTON) == False:
-        	print('Button Pressed')
-        	time.sleep(0.2)
+    while True:
+        if _input(BUTTON) == False:
+            print('Button Pressed')
+            time.sleep(0.2)
