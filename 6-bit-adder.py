7104d6d3ed46f15dd196f0fe32b3bdf99117f1ec
Hermann Krumrey <hermann@krumreyh.com>
2017-01-11 14:16:17 +0100

    Add Program logic

--- 6-bit-adder.py
+++ 6-bit-adder.py
@@ -29,6 +29,9 @@ TOP_SUB_BUTTON = 13
 BOT_ADD_BUTTON = 19
 BOT_SUB_BUTTON = 26
 
+TOP_VALUE = 0
+MID_VALUE = 0
+
 
 def setup_gpio():
     setmode(BCM)
@@ -48,13 +51,90 @@ def cleanup():
     for led in TOP_ROW + MIDDLE_ROW + BOTTOM_ROW:
         output(led, LOW)
 
+def draw_number(number, row):
+
+	binary = convert_to_binary(number)
+
+	while len(binary) < len(row):
+		binary.append(0)
+
+	for i in range(0, len(row)):
+
+		if binary[i] == 1:
+			output(row[i], HIGH)
+		else:
+			output(row[i], LOW)
+
+
+def convert_to_binary(number):
+
+	binary_numbers = []
+
+	while int(number / 2) >= 1:
+		binary_numbers.append(number % 2)
+		number = int(number / 2)
+
+	binary_numbers.append(number % 2)
+
+	return list(reversed(binary_numbers))
+
+def wait_for_input():
+
+	while True:
+
+		if not _input(TOP_ADD_BUTTON):
+			return "TOP_ADD"
+		elif not _input(TOP_SUB_BUTTON):
+			return "TOP_SUB"
+		elif not _input(BOT_ADD_BUTTON):
+			return "MID_ADD"
+		elif not _input(BOT_SUB_BUTTON):
+			return "MID_SUB"
+		else:
+			pass
+
+def increment(number):
+	if number < 63:
+		return number + 1
+	else:
+		return number
+
+def decrement(number):
+	if number > 0:
+		return number - 1
+	else:
+		return number
+
+def process_command(command):
+
+	if command == "TOP_ADD":
+		TOP_VALUE = increment(TOP_VALUE)
+	elif command == "TOP_SUB"
+		TOP_VALUE = decrement(TOP_VALUE)
+	elif command == "MID_ADD":
+		MID_VALUE = increment(MID_VALUE)
+	elif command == "MID_SUB"
+		MID_VALUE = decrement(MID_VALUE)
+
+def refresh():
+
+	draw_number(TOP_VALUE, TOP_ROW)
+	draw_number(MID_VALUE, MIDDLE_ROW)
+	draw_number(TOP_VALUE + MID_VALUE, BOTTOM_ROW)
+
+
+def main():
+
+	while True:
+		command = wait_for_input()
+		process_command(command)
+		refresh()
+
 
 if __name__ == "__main__":
 
 	setup_gpio()
-
-	for led in TOP_ROW + MIDDLE_ROW + BOTTOM_ROW:
-        output(led, HIGH)
-        time.sleep(1)
-
-    cleanup()
+	try:
+		main()
+	except KeyboardInterrupt:
+    	cleanup()
