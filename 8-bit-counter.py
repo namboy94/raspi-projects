a07bd2f24b4d282d85a8b6095cda7bf141e8542e
Hermann Krumrey <hermann@krumreyh.com>
2017-01-12 10:03:06 +0100

    Preliminary Fix while testing with Hardware

--- 8-bit-counter.py
+++ 8-bit-counter.py
@@ -39,7 +39,7 @@ def setup_gpio():
         setup(led, OUT)
 
     for segment in LSD + MSD:
-    	setup(segment, OUT)
+        setup(segment, OUT)
 
     setup(BEEPER, OUT)
     setup(BUTTON, IN, pull_up_down=PUD_UP)
@@ -53,76 +53,76 @@ def cleanup():
         output(led, LOW)
 
     for segment in LSD + MSD:
-    	output(segment, LOW)
+        output(segment, LOW)
 
     output(BEEPER, LOW)
 
 
 def draw(number):
-	binary = convert_to_8_bit_binary(number)
+    binary = convert_to_8_bit_binary(number)
 
-	for i in range(0, 8):
+    for i in range(0, 8):
 
-		if binary[i] == 1:
-			output(BIT_LEDS[i], HIGH)
-		else:
-			output(BIT_LEDS[i], LOW)
+        if binary[i] == 1:
+            output(BIT_LEDS[i], HIGH)
+        else:
+            output(BIT_LEDS[i], LOW)
 
-	numberstring = str(number).zfill(2)
+    numberstring = str(number).zfill(2)
 
-	draw_lcd(int(numberstring[-1]), LSD)
-	draw_lcd(int(numberstring[-2]), MSD)
+    draw_lcd(int(numberstring[-1]), LSD)
+    draw_lcd(int(numberstring[-2]), MSD)
 
-	if len(numberstring > 2):
+    if len(numberstring) > 2:
 
-		if numberstring[0] == "1":
-			output(ONE_HUNDRED_LED, HIGH)
-			output(TWO_HUNDRED_LED, LOW)
+        if numberstring[0] == "1":
+            output(ONE_HUNDRED_LED, HIGH)
+            output(TWO_HUNDRED_LED, LOW)
 
-		elif numberstring[0] == "2":
-			output(ONE_HUNDRED_LED, HIGH)
-			output(TWO_HUNDRED_LED, HIGH)
+        elif numberstring[0] == "2":
+            output(ONE_HUNDRED_LED, HIGH)
+            output(TWO_HUNDRED_LED, HIGH)
 
-	else:
-		output(ONE_HUNDRED_LED, LOW)
-		output(TWO_HUNDRED_LED, LOW)
+    else:
+        output(ONE_HUNDRED_LED, LOW)
+        output(TWO_HUNDRED_LED, LOW)
 
 
 def draw_lcd(digit, lcd):
 
-	active = []
-	if digit == 0:
-		active = [0, 1, 2, 3, 4, 5]
-	elif digit == 1:
-		active = [0, 3]
-	elif digit == 2:
-		active = [1, 2, 3, 4, 6]
-	elif digit == 3:
-		active = [0, 1, 3, 4, 6]
-	elif digit == 4:
-		active = [0, 3, 6, 5]
-	elif digit == 5:
-		active = [0, 1, 4, 5, 6]
-	elif digit == 6:
-		active = [0, 1, 2, 4, 5, 6]
-	elif digit == 7:
-		active = [0, 3, 4]
-	elif digit == 8:
-		active = [0, 1, 2, 3, 4, 5, 6]
-	elif digit == 9:
-		active = [0, 3, 4, 5, 6]
+    active = []
+    if digit == 0:
+        active = [0, 1, 2, 3, 4, 5]
+    elif digit == 1:
+        active = [0, 3]
+    elif digit == 2:
+        active = [1, 2, 3, 4, 6]
+    elif digit == 3:
+        active = [0, 1, 3, 4, 6]
+    elif digit == 4:
+        active = [0, 3, 6, 5]
+    elif digit == 5:
+        active = [0, 1, 4, 5, 6]
+    elif digit == 6:
+        active = [0, 1, 2, 4, 5, 6]
+    elif digit == 7:
+        active = [0, 3, 4]
+    elif digit == 8:
+        active = [0, 1, 2, 3, 4, 5, 6]
+    elif digit == 9:
+        active = [0, 3, 4, 5, 6]
 
-	for segment in range(0, 7):
-		if segment in active:
-			output(lcd[segment], HIGH)
-		else:
-			output(lcd[segment], LOW)
+    for segment in range(0, 7):
+        if segment in active:
+            output(lcd[segment], HIGH)
+        else:
+            output(lcd[segment], LOW)
 
 
 def convert_to_8_bit_binary(number):
 
-	if number > 255:
-		return [1, 1, 1, 1, 1, 1, 1, 1]
+    if number > 255:
+        return [1, 1, 1, 1, 1, 1, 1, 1]
 
     binary_numbers = []
 
@@ -133,44 +133,44 @@ def convert_to_8_bit_binary(number):
     binary_numbers.append(number % 2)
 
     while len(binary_numbers) < 8:
-    	binary_numbers.append(0)
+        binary_numbers.append(0)
 
     return list(reversed(binary_numbers))
 
 def wait_for_input():
-	while _input(BUTTON):
-		pass
+    while _input(BUTTON):
+        pass
 
 
 class Counter(object):
 
-	def __init__():
-		self.count = 0
+    def __init__(self):
+        self.count = 0
 
-	def increment():
-		if self.count < 255:
-			self.count += 1
-		draw(self.count)
+    def increment(self):
+        if self.count < 255:
+            self.count += 1
+        draw(self.count)
 
-	def reset():
-		self.count = 0
-		draw(self.count)
+    def reset(self):
+        self.count = 0
+        draw(self.count)
 
 
 if __name__ == "__main__":
 
-	setup_gpio()
-	try:
-		counter = Counter()
-		while True:
+    setup_gpio()
+    try:
+        counter = Counter()
+        while True:
 
-			wait_for_input()
-			output(BEEPER, HIGH)
+            wait_for_input()
+            output(BEEPER, HIGH)
 
-			counter.increment()
-			time.sleep(0.1)
+            counter.increment()
+            time.sleep(0.1)
 
-			output(BEEPER, LOW)
+            output(BEEPER, LOW)
 
-	except KeyboardInterrupt:
-		cleanup()
+    except KeyboardInterrupt:
+        cleanup()
